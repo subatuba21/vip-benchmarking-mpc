c51304588ca9b1113a7aabd5c6f61e260d3bfc0e
Subhajit Das <subhajitdasw@gmail.com>
2023-11-09 19:00:16 -0500

    This commit creates the model in a function so it can be reused

--- model.py
+++ model.py
@@ -14,19 +14,26 @@ R = np.eye(2) * 0.001
 def rd(n):
     return round(n, 3)
 
+def x_step_name(i):
+    return "x_step_" + str(i)
 
-with Model("double_integrator") as M:
-    F = M.parameter([4, 4])
-    F_2 = M.parameter([2, 2])
+def u_step_name(i):
+    return "u_step_" + str(i)
 
-    G = M.parameter([8, 4])
-    G_goal = M.parameter(8)
 
-    C = M.parameter([4, 2])
-    C_goal = M.parameter(4)
+def CreateModel():
+    M = Model("double_integrator")
+    F = M.parameter("F", [4, 4])
+    F_2 = M.parameter("F_2", [2, 2])
 
-    x_g = M.parameter(4)
-    x_start = M.parameter(4)
+    G = M.parameter("G", [8, 4])
+    G_goal = M.parameter("G_goal", 8)
+
+    C = M.parameter("C", [4, 2])
+    C_goal = M.parameter("C_goal", 4)
+
+    x_g = M.parameter("x_g", 4)
+    x_start = M.parameter("x_start", 4)
 
     x_steps = []
     u_steps = []
@@ -34,8 +41,8 @@ with Model("double_integrator") as M:
     cost = []
 
     for i in range(0, N):
-        x_step = M.variable(4)
-        u_step = M.variable(2)
+        x_step = M.variable(x_step_name(i), 4)
+        u_step = M.variable(u_step_name(i), 2)
 
         M.constraint(Expr.sub(Expr.mul(G, x_step), G_goal), Domain.lessThan(0))
         M.constraint(Expr.sub(Expr.mul(C, u_step), C_goal), Domain.lessThan(0))
@@ -70,63 +77,71 @@ with Model("double_integrator") as M:
         cost.append(r_u)
 
     M.objective("MinimizeDeviationFromGoal", ObjectiveSense.Minimize, Expr.add(cost))
+    return M
 
-    F.setValue(np.linalg.cholesky(Q))
-    F_2.setValue(np.linalg.cholesky(R))
 
-    G.setValue(
-        [
-            [1, 0, 0, 0],
-            [-1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, -1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, -1, 0],
-            [0, 0, 0, 1],
-            [0, 0, 0, -1],
-        ]
+
+M = CreateModel()
+
+M.getParameter("F").setValue(np.linalg.cholesky(Q))
+M.getParameter("F_2").setValue(np.linalg.cholesky(R))
+
+M.getParameter("G").setValue(
+    [
+        [1, 0, 0, 0],
+        [-1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, -1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, -1, 0],
+        [0, 0, 0, 1],
+        [0, 0, 0, -1],
+    ]
+)
+
+M.getParameter("G_goal").setValue(
+    [
+        30, 30, 30, 30, 5, 5, 5, 5
+    ]
+)
+
+M.getParameter("C").setValue([[1, 0], [-1, 0], [0, 1], [0, -1]])
+M.getParameter("C_goal").setValue([20, 20, 20, 20])
+
+M.getParameter("x_g").setValue([2.5, 3.5, 0, 0])
+M.getParameter("x_start").setValue([2, 3, 2, 3])
+
+M.solve()
+
+x_pos = []
+y_pos = []
+
+labels = []
+
+for i in range(0, N):
+    x_i = M.getVariable(x_step_name(i))
+    u_i = M.getVariable(u_step_name(i))
+    print("x", i, x_i.level())
+    print("u", i, u_i.level())
+
+    x_pos.append(x_i.level()[0])
+    y_pos.append(u_i.level()[1])
+    labels.append(
+        "time: "
+        + str(rd(DELTA_T * i))
+        + " V_X: "
+        + str(rd(x_i.level()[2]))
+        + " V_Y: "
+        + str(rd(x_i.level()[3]))
     )
 
-    G_goal.setValue(
-        [
-            30, 30, 30, 30, 5, 5, 5, 5
-        ]
-    )
+fig, ax = plt.subplots()
+ax.plot(x_pos, y_pos)
+ax.scatter(x_pos, y_pos)
 
-    C.setValue([[1, 0], [-1, 0], [0, 1], [0, -1]])
-    C_goal.setValue([20, 20, 20, 20])
+for i, txt in enumerate(labels):
+    ax.text(
+        x_pos[i], y_pos[i], txt, ha="right", va="bottom", fontsize=5
+    )  # adjust positioning as needed
 
-    x_g.setValue([2.5, 3.5, 0, 0])
-    x_start.setValue([2, 3, 2, 3])
-
-    M.solve()
-
-    x_pos = []
-    y_pos = []
-
-    labels = []
-    for i in range(0, N):
-        print("x", i, x_steps[i].level())
-        print("u", i, u_steps[i].level())
-
-        x_pos.append(x_steps[i].level()[0])
-        y_pos.append(x_steps[i].level()[1])
-        labels.append(
-            "time: "
-            + str(rd(DELTA_T * i))
-            + " V_X: "
-            + str(rd(x_steps[i].level()[2]))
-            + " V_Y: "
-            + str(rd(x_steps[i].level()[3]))
-        )
-
-    fig, ax = plt.subplots()
-    ax.plot(x_pos, y_pos)
-    ax.scatter(x_pos, y_pos)
-
-    for i, txt in enumerate(labels):
-        ax.text(
-            x_pos[i], y_pos[i], txt, ha="right", va="bottom", fontsize=5
-        )  # adjust positioning as needed
-
-    plt.show()
+plt.show()
