f44afd20800056825adad4654146c22864d11188
Subhajit Das <subhajitdasw@gmail.com>
2023-11-30 17:27:01 -0500

    This commit adds comments to the parameters of the model

--- model.py
+++ model.py
@@ -2,15 +2,6 @@ from mosek.fusion import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-N = 10
-DELTA_T = 0.1
-
-# goal = [10, 0, 0, 0]
-Q = np.eye(4)
-R = np.eye(2) * 0.001
-
-
 def rd(n):
     return round(n, 3)
 
@@ -21,21 +12,33 @@ def u_step_name(i):
     return "u_step_" + str(i)
 
 
-def CreateModel():
+def CreateModel(DELTA_T, N):
     M = Model("double_integrator")
+
+    # Decomposition of Q
     F = M.parameter("F", [4, 4])
+
+    # Decomposition of R
     F_2 = M.parameter("F_2", [2, 2])
 
+    # Multiply x_step with G to get speeds/position, 1 is max, -1 is min, then apply constraints to make sure each fits within the G_goal
     G = M.parameter("G", [8, 4])
+
+    # Constraints on max and min values on X
     G_goal = M.parameter("G_goal", 8)
 
+    # Multiply u_step with U to get acceleration, 1 is max, -1 is min, then apply constraints to make sure each fits within the U_goal
     C = M.parameter("C", [4, 2])
     C_goal = M.parameter("C_goal", 4)
 
+    # states that represent the goal and start values
     x_g = M.parameter("x_g", 4)
     x_start = M.parameter("x_start", 4)
 
+    # timesteps of x
     x_steps = []
+
+    # timesteps of u
     u_steps = []
 
     cost = []
@@ -81,67 +84,126 @@ def CreateModel():
 
 
 
-M = CreateModel()
+def initial_test_run():
+    Q = np.eye(4)
+    R = np.eye(2) * 0.001
 
-M.getParameter("F").setValue(np.linalg.cholesky(Q))
-M.getParameter("F_2").setValue(np.linalg.cholesky(R))
+    DELTA_T = 0.1
+    N = 10
+    M = CreateModel(DELTA_T, N)
 
-M.getParameter("G").setValue(
-    [
-        [1, 0, 0, 0],
-        [-1, 0, 0, 0],
-        [0, 1, 0, 0],
-        [0, -1, 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, -1, 0],
-        [0, 0, 0, 1],
-        [0, 0, 0, -1],
-    ]
-)
+    M.getParameter("F").setValue(np.linalg.cholesky(Q))
+    M.getParameter("F_2").setValue(np.linalg.cholesky(R))
 
-M.getParameter("G_goal").setValue(
-    [
-        30, 30, 30, 30, 5, 5, 5, 5
-    ]
-)
-
-M.getParameter("C").setValue([[1, 0], [-1, 0], [0, 1], [0, -1]])
-M.getParameter("C_goal").setValue([20, 20, 20, 20])
-
-M.getParameter("x_g").setValue([2.5, 3.5, 0, 0])
-M.getParameter("x_start").setValue([2, 3, 2, 3])
-
-M.solve()
-
-x_pos = []
-y_pos = []
-
-labels = []
-
-for i in range(0, N):
-    x_i = M.getVariable(x_step_name(i))
-    u_i = M.getVariable(u_step_name(i))
-    print("x", i, x_i.level())
-    print("u", i, u_i.level())
-
-    x_pos.append(x_i.level()[0])
-    y_pos.append(u_i.level()[1])
-    labels.append(
-        "time: "
-        + str(rd(DELTA_T * i))
-        + " V_X: "
-        + str(rd(x_i.level()[2]))
-        + " V_Y: "
-        + str(rd(x_i.level()[3]))
+    M.getParameter("G").setValue(
+        [
+            [1, 0, 0, 0],
+            [-1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, -1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, -1, 0],
+            [0, 0, 0, 1],
+            [0, 0, 0, -1],
+        ]
     )
 
-fig, ax = plt.subplots()
-ax.plot(x_pos, y_pos)
-ax.scatter(x_pos, y_pos)
+    M.getParameter("G_goal").setValue(
+        [
+            30, 30, 30, 30, 5, 5, 5, 5
+        ]
+    )
 
-for i, txt in enumerate(labels):
-    ax.text(
-        x_pos[i], y_pos[i], txt, ha="right", va="bottom", fontsize=5
-    )  # adjust positioning as needed
+    M.getParameter("C").setValue([[1, 0], [-1, 0], [0, 1], [0, -1]])
+    M.getParameter("C_goal").setValue([20, 20, 20, 20])
 
-plt.show()
+    M.getParameter("x_g").setValue([2.5, 3.5, 0, 0])
+    M.getParameter("x_start").setValue([2, 3, 2, 3])
+
+    M.solve()
+
+    x_pos = []
+    y_pos = []
+
+    labels = []
+
+    for i in range(0, N):
+        x_i = M.getVariable(x_step_name(i))
+        u_i = M.getVariable(u_step_name(i))
+        print("x", i, x_i.level())
+        print("u", i, u_i.level())
+
+        x_pos.append(x_i.level()[0])
+        y_pos.append(u_i.level()[1])
+        labels.append(
+            "time: "
+            + str(rd(DELTA_T * i))
+            + " V_X: "
+            + str(rd(x_i.level()[2]))
+            + " V_Y: "
+            + str(rd(x_i.level()[3]))
+        )
+
+    fig, ax = plt.subplots()
+    ax.plot(x_pos, y_pos)
+    ax.scatter(x_pos, y_pos)
+
+    for i, txt in enumerate(labels):
+        ax.text(
+            x_pos[i], y_pos[i], txt, ha="right", va="bottom", fontsize=5
+        )  # adjust positioning as needed
+
+    plt.show()
+
+
+
+def randomized_test_run(num_tests, numRandomPlotsShown):
+    models = []
+    for i in range(0, num_tests):
+        Q = np.eye(4)
+        R = np.eye(2) * np.random.random() * 1
+
+        DELTA_T = np.random.random() * 0.5
+        N = 10
+        M = CreateModel(DELTA_T, N)
+
+        M.getParameter("F").setValue(np.linalg.cholesky(Q))
+        M.getParameter("F_2").setValue(np.linalg.cholesky(R))
+
+        M.getParameter("G").setValue(
+            [
+                [1, 0, 0, 0],
+                [-1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, -1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, -1, 0],
+                [0, 0, 0, 1],
+                [0, 0, 0, -1],
+            ]
+        )
+
+        M.getParameter("G_goal").setValue(
+            [
+                np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30)
+            ]
+        )
+
+        M.getParameter("C").setValue([[1, 0], [-1, 0], [0, 1], [0, -1]])
+        M.getParameter("C_goal").setValue([np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30)])
+
+        M.getParameter("x_g").setValue([np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30)])
+        M.getParameter("x_start").setValue([np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30)])
+
+        M.setSolverParam("numThreads", 1)
+        models.append(M)
+
+    Model.solveBatch(False, -1.0, 4, models)
+
+    for
+
+    
+
+
+# initial_test_run()
+randomized_test_run(1000)
